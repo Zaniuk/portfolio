3486ba062c61347f99e5db10126e54d785b4d707
Zaniuk <geronimo.zaniuk2000@gmail.com>
2022-06-14 14:04:59 -0300

    add styles to Landing component

--- src/components/Landing/Landing.svelte
+++ src/components/Landing/Landing.svelte
@@ -1,11 +1,12 @@
 <script>
     import ReactWindow from './ReactWindow/ReactWindow.svelte'
+    import 'animate.css'
 </script>
 
 <section id="home">
 
-    <h1>Welcome, $&#123;Visitor!&#125;</h1>
-    <h1>Thank you for visiting my site</h1>
+    <h1 ><span class="text-lblue">Welcome,</span><span class="text-pink">$&#123;</span><span class="text-lblue">'</span><span class="text-green">Visitor!</span><span class="text-lblue">'</span><span class="text-pink">&#125;</span></h1>
+    <h1><span class="text-pink">Thank</span> <span class="text-ulblue">you</span> <span class="text-pink">for</span> <span class="text-lblue">visiting </span><span class="text-green">my site</span></h1>
     <button >Run Welcome.js</button>
     <div>
         
@@ -21,8 +22,19 @@
 </section>
 
 <style>
+    h1{
+        padding-top: .5rem;
+        padding-bottom: .5rem;
+        margin: 0;
+    }
     section{
         height: 100vh;
+    }
+    #home{
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: center;
+        align-items: center;
     }
     article img{
         height: 24px;
@@ -30,12 +42,27 @@
     #react-window{
         display: flex;
         text-align: center;
+        border-radius: 8px 8px 0px 0px;
     }
     #react-window span{
         margin: 0 auto;
     }
     article{
-        
+        box-sizing: border-box;
+        width: 380px;
         display: inline-block;
+        border-radius: 8px;
+    }
+    @media (max-width: 375px) {
+        article{
+            font-size: .7rem;
+            width: 100%;
+        }
+    }
+    @media (max-width: 310) {
+        article{
+            font-size: .55rem;
+            width: 100%;
+        }
     }
 </style>
