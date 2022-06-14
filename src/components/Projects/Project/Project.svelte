c12ee57a610c1c69273766f7b11b9fd8312a0e45
Zaniuk <geronimo.zaniuk2000@gmail.com>
2022-06-14 14:14:33 -0300

    created a more reusable Project card

--- src/components/Projects/Project/Project.svelte
+++ src/components/Projects/Project/Project.svelte
@@ -1,10 +1,35 @@
 <script>
-    export let project
+    export let project;
+    export let linkGithub;
 </script>
-<h1>{project}</h1>
-<div>
+
+<div class="project">
+    <h1>{project}</h1>
     <img src="https://i.picsum.photos/id/356/200/200.jpg?hmac=Pd7TXMbO4gSTwhtmub1DcSo1vPpeCVRsuY_BRE_llmU" alt="random">
-    <button>Github</button>
-    <button>About</button>
-    <button>Demo</button>
+    <div class="buttons">
+        <a class="text-lblue" href={linkGithub} target="_blank">Github</a>
+        <a class="text-green" href="...">About</a>
+        <a class="text-pink" href="...">Demo</a>
+    </div>
 </div>
+<style>
+    .project{
+        max-width: 400px;
+        display: flex;
+        flex-flow: column wrap;
+    }
+    img{
+        width:100%;
+        aspect-ratio: 1;
+    }
+    .buttons{
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: space-around;
+    }
+    a{
+        padding-top: .5rem;
+        padding-bottom: .5rem;
+        text-decoration: none;
+    }
+</style>
--- src/components/Projects/Projects.svelte
+++ src/components/Projects/Projects.svelte
@@ -6,7 +6,25 @@
 <pre><code>
 <h1>> npm run showMyProjects</h1>
 </code></pre>
-    <Project project={'The Wine Valley'}/>
-    <Project project={'Blog Front-End'}/>
-    <Project project={'Blog Back-End'}/>
+<div id="projects-container">
+    <Project project={'Blog Front-End'} linkGithub={'https://github.com/Zaniuk/Blog'} />
+    <Project project={'Blog Back-End'} linkGithub={'https://github.com/Zaniuk/Blog-backend'} />
+    <Project project={'The Wine Valley'} linkGithub={'https://github.com/Zaniuk/TiendaVinos'} />
+</div>
 </section>
+<style>
+    #projects-container{
+        max-width: 900px;
+        margin: 0 auto;
+        display: grid;
+        align-items: center;
+        justify-items: center;
+        grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    }
+    h1{
+        text-align: center;
+        padding: 0;
+        margin: 0;
+        padding-top: 4rem;
+}
+</style>
