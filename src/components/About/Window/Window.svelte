a231df72a78372900cb2010f2c0865998a161394
Zaniuk <geronimo.zaniuk2000@gmail.com>
2022-06-14 14:02:36 -0300

    created a more reusable component

---
+++ src/components/About/Window/Window.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let text;
+    export let title;
+</script>
+
+    <div>
+        
+        <article class="bg-light">
+            <div id="react-window" class="bg-window">
+                <img src="dots.svg" alt="aa">
+                <span>{title}</span>
+            </div>
+            <p>
+                {text}
+            </p>            
+        </article>
+    </div>
+    
+
+<style>
+    article img{
+        height: 24px;
+    }
+    #react-window{
+        display: flex;
+        text-align: center;
+        border-radius: 8px 8px 0px 0px;
+    }
+    #react-window span{
+        margin: 0 auto;
+    }
+    article{
+        width: 380px;
+        display: inline-block;
+        border-radius: 8px;
+        height: 300px;
+    }
+    @media (max-width: 375px) {
+        article{
+            font-size: .7rem;
+            width: 100%;
+        }
+    }
+    @media (max-width: 310) {
+        article{
+            font-size: .55rem;
+            width: 100%;
+        }
+    }
+</style>
