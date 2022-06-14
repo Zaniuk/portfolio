1031597e731829c51f8a98c301dcec11208e7264
Zaniuk <geronimo.zaniuk2000@gmail.com>
2022-06-14 14:05:55 -0300

    deleted component to create a more reusable one

--- src/components/About/AboutWindow/AboutWindow.svelte
+++
@@ -1,3 +0,0 @@
-<p>
-    I'm a looking-forward, colaborative, responsible  and highly selft-taugh person. A huge passionate of technology and programming, and continum learning. I'm searching for challenges that pushes me in to a continum personal, academic and professional growth.
-</p>
