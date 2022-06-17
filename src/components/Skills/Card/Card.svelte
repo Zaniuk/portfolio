23b6d3c47c1e5d4951b2ad43e9ecf324a5b0cd6d
Zaniuk <geronimo.zaniuk2000@gmail.com>
2022-06-17 02:22:18 -0300

    fixed img sizing

--- src/components/Skills/Card/Card.svelte
+++ src/components/Skills/Card/Card.svelte
@@ -1,13 +1,42 @@
 <script>
     export let technology;
     export let color1;
+    export let img;
+    export let exc;
 </script>
 <article>
     <h1 class={color1}>{technology}</h1>
-    <img src="https://i.picsum.photos/id/356/200/200.jpg?hmac=Pd7TXMbO4gSTwhtmub1DcSo1vPpeCVRsuY_BRE_llmU" alt="random">
+    <div class="img-wrapper bg-light">
+        <img class={exc} src={img} alt="random">
+    </div>
 </article>
 <style>
     h1{
         text-align: center;
+        margin-top: .5rem;
+        margin-bottom: .5rem;
+    }
+
+    article{
+        box-sizing: border-box;
+        padding-top: .5rem;
+        padding-bottom: .5rem;
+        padding-right: .2rem;
+        padding-left: .2rem;
+    }
+    .img-wrapper{
+        width: 240px;
+        height: 240px;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        padding: .5rem;
+        border-radius: .5rem;
+    }
+    .img-wrapper img{
+        width: 90%;
+        height: auto;
     }
 </style>
