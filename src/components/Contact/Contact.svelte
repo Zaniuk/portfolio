f22ceb13cdcd57b60cba3c50247c1fa50078d8ea
Zaniuk <geronimo.zaniuk2000@gmail.com>
2022-06-17 02:20:33 -0300

    changed caret color and fixed css centering

--- src/components/Contact/Contact.svelte
+++ src/components/Contact/Contact.svelte
@@ -2,23 +2,95 @@
     
 </script>
 <section id="contact">
+    <h1><span class="text-purple">></span> <span class="text-green">node</span> <span class="text-pink">contact</span>.<span class="text-yellow">js</span></h1>
     <form>
         <span>
-            Name
-            <input type="text" name="" id="" required>
+            <b class="text-pink">Name</b>
+            <input class="border-pink caret-pink"  type="text" name="" id="name-input" placeholder="Show me how much awsome your name is!!">
         </span>
         <span>
-            Email
-            <input type="text" name="" id="" required>
+            <b class="text-green">Email</b>
+            <input class="border-green caret-green"  type="email" name="" id="email-input" required placeholder="Put a beatiful email here...">
         </span>
         <span>
-            Company (optional)
-            <input type="text" name="" id="">
+            <b class="text-lblue">Company (optional)</b>
+            <input class="border-lblue caret-lblue"  type="text" name="" id="company-input" placeholder="Talking from a company? 👀">
         </span>
         <span>
-            Message
-            <textarea name="" id="" cols="30" rows="10" required></textarea>
+            <b class="text-purple">Message</b>
+            <textarea class="border-purple caret-purple" name="" id="message-input" cols="30" rows="10" required></textarea>
         </span>
-        <button>Send</button>
+        <div class="send-button">
+            <button class="text-lblue border-lblue">Send</button>
+        </div>
     </form>
 </section>
+
+<style>
+    h1{
+        text-align: center;
+    }
+    h1 span{
+        display: inline;
+    }
+    b{
+        font-weight: normal;
+    }
+    form{
+        display: flex;
+        flex-flow: column nowrap;
+        /*margin: .5rem;*/
+        
+    }
+    form span{
+        display: flex;
+        flex-flow: column nowrap;
+        width: 95%;
+        margin: 0 auto;
+    }
+    @media (min-width: 600px) {
+        form span{
+            width: 90%;
+            margin: 0 auto;
+        }
+    }
+    @media (min-width: 1000px) {
+        form span{
+            width: 900px;
+            margin: 0 auto;
+        }
+    }
+    input{
+        color: white;
+        background-color: #1A1B26;
+        border-radius: .3rem;
+        padding: .5rem;
+        height: 1.5rem;
+    }
+    textarea{
+        color: white;
+        resize: none;
+        background-color: #1A1B26;
+        border-radius: .3rem;
+    }
+    b{
+        padding-top: .5rem;
+        padding-bottom: .5rem;
+    }
+    .send-button{
+        display: flex;
+        justify-content: center;
+        margin-top: 1rem;
+    }
+    .send-button button{
+        background-color: #1A1B26;
+        padding: .5rem 2rem;
+        font-size: 1.2rem;
+        border-radius: .5rem;
+        transition: 250ms ease-in-out;
+    }
+    .send-button button:hover{
+        background-color: var(--lblue);
+        color: white;
+    }
+</style>
