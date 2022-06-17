1e2c5ed94501afb0af8d16de71e3b01c95b91bf7
Zaniuk <geronimo.zaniuk2000@gmail.com>
2022-06-17 02:22:59 -0300

    added img prop to reuse with different images

--- src/components/Skills/Skills.svelte
+++ src/components/Skills/Skills.svelte
@@ -7,12 +7,12 @@ import Card from './Card/Card.svelte'
 <h1><span class="text-pink">console</span>.<span class="text-lblue">log</span><span class="text-ulblue">(</span><span class="text-green">'My Skills'</span><span class="text-ulblue">)</span>;</h1>
 
 <div>
-        <Card color1={'text-green'}  technology={'NodeJS'}/>
-        <Card color1={'text-purple'}  technology={'React'}/>
-        <Card color1={'text-ulblue'}  technology={'MongoDB'}/>
-        <Card color1={'text-lblue'}  technology={'ExpressJS'}/>
-        <Card color1={'text-yellow'}  technology={'JavaScript'}/>
-        <Card color1={'text-pink'}  technology={'HTML/CSS'}/>
+        <Card color1={'text-green'}  technology={'NodeJS'} img={'nodejs.png'}/>
+        <Card color1={'text-purple'}  technology={'React'} img={'react.png'}/>
+        <Card color1={'text-ulblue'}  technology={'MongoDB'} img={'mongo.png'}/>
+        <Card color1={'text-lblue'}  technology={'ExpressJS'} img={'express.png'}/>
+        <Card color1={'text-yellow'}  technology={'JavaScript'} img={'js.png'}/>
+        <Card color1={'text-pink'}  technology={'HTML/CSS'} img={'htmlcss.png'} exc={'html-img'}/>
 </div>
 </section>
 <style>
