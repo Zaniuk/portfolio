c0f5b3b6d39595a8337778b97f6361e6dba69760
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-06-19 13:28:39 -0300

    added observer and animations to Projects section

--- src/components/Projects/Projects.svelte
+++ src/components/Projects/Projects.svelte
@@ -1,14 +1,34 @@
 <script>
-    import Project from './Project/Project.svelte'
-    
+import { onMount } from 'svelte';
+
+import Project from './Project/Project.svelte'
+    onMount(()=>{
+        const project1 = document.getElementById('project-1')
+        const project2 = document.getElementById('project-2')
+        const projectsSection = document.getElementById('projects')
+        const options= {
+        root: null,
+        threshold: 0.2
+    }
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if(entry.isIntersecting == true){
+                project1.classList.add('slide-in-fwd-top')
+                project2.classList.add('slide-in-fwd-top-delay-4')
+            }
+        })
+    }, options)
+    observer.observe(projectsSection)
+    })
 </script>
 <section id="projects">
 <pre><code>
 <h1>> npm run showMyProjects</h1>
 </code></pre>
 <div id="projects-container">
-    <Project project={'Blog Back-End'} linkGithub={'https://github.com/Zaniuk/Blog-backend'} linkDemo={'https://ancient-badlands-91189.herokuapp.com/api/posts'} img={'blog.png'} />
-    <Project project={'The Wine Valley'} linkGithub={'https://github.com/Zaniuk/TiendaVinos'} linkDemo={'https://winevalley.pages.dev/'}  img={'wine.png'} />
+    <Project id="project-1" project={'Blog Back-End'} linkGithub={'https://github.com/Zaniuk/Blog-backend'} linkDemo={'https://ancient-badlands-91189.herokuapp.com/api/posts'} img={'blog.png'} />
+    <Project id="project-2" project={'The Wine Valley'} linkGithub={'https://github.com/Zaniuk/TiendaVinos'} linkDemo={'https://winevalley.pages.dev/'}  img={'wine.png'} />
 </div>
 </section>
 <style>
