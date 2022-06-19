61e8a70c1fecf7dcee86f188ce22448bcbd3cdf9
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-06-18 22:44:50 -0300

    added sucess route for contact form

---
+++ src/components/Sucess/Sucess.svelte
@@ -0,0 +1,17 @@
+<section>
+        <img src="sucess.png" alt="">
+        <h1>Sucess!</h1>
+        <h2>Your message has successfully been sent.</h2>
+        <p>I will contact you soon as possible.</p>
+</section>
+<style>
+    section{
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+        align-items: center;
+    }
+    img{
+        width: 100px;
+    }
+</style>
---
+++ src/routes/sucess.svelte
@@ -0,0 +1,8 @@
+<script>
+    import '../css/global.css'
+    import Sucess from '../components/Sucess/Sucess.svelte'
+</script>
+<svelte:head>
+    <title>Sucess!</title>
+</svelte:head>
+<Sucess/>
