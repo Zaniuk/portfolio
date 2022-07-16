eb07890e22c5cb86f17612cbddc5ad538057a7aa
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-16 15:56:22 -0300

    Added some styles to login page

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -41,22 +41,77 @@
 
 <form>
     <label>
-        Email
+        <p id="p-email">Email</p>
         <input id="email" type="email" required>
     </label>
 
     <label>
-        Password
+        <p id="p-pass">Password</p>
         <input id="password" type="password" required>
     </label>
     <label>
         <input id="submit" type="submit" value="Login">
     </label>
 </form>
-<h1>Hola login</h1>
 
 <style>
+    @import '../css/global.css';
+    :root{
+    --lblue: #3C91E6;
+    --green: #75B150;
+    --pink: #AE415C;
+    --purple: #A78ADD;
+    --yellow: #FFF500;
+    --white: white;
+    --ulblue: #7BCFCE;
+    --bg-dark: #16161E;
+    --bg-light: #1A1B26 ;
+    --bg-window:rgba(217, 217, 217, .03);
+}
     label {
         display: block;
+    }
+    #p-email{
+        color: var(--lblue);
+    }
+    #p-pass{
+        color: var(--pink);
+    }
+    #email{
+        padding: .5rem;
+        width: 280px;
+        border: 2px solid var(--lblue);
+        color: var(--lblue);
+        background-color: var(--bg-light);
+        border-radius: .3rem;
+        transition: 100ms ease-in-out
+    }
+    #password{
+        padding: .5rem;
+        width: 280px;
+        border: 2px solid var(--pink);
+        color: var(--pink);
+        background-color: var(--bg-light);
+        border-radius: .3rem;
+        transition: 100ms ease-in-out
+    }
+    #submit{
+        background-color: var(--bg-light);
+        color: var(--purple);
+        border: 2px solid var(--purple);
+        padding: .5rem 1rem;
+        margin: 1rem;
+        border-radius: .3rem;
+        transition: 250ms ease-in-out;
+    }
+    #submit:hover, #submit:focus{
+        background-color: var(--purple);
+        color: white;
+    }
+    form{
+        margin-top: 20vh;
+        display: flex;
+        flex-flow: column;
+        align-items: center;
     }
 </style>
