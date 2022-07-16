38702901211c30c708de346f29bd8f7cbf1a3e32
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-15 21:56:49 -0300

    added navbar (work in progress)

---
+++ src/components/Blog/Navbar/Navbar.svelte
@@ -0,0 +1,13 @@
+<nav>
+    <br><br><br>
+</nav>
+
+<style>
+    nav{
+        box-sizing: border-box;
+        background-color: #0c0c0f;
+        padding: .5rem;
+        min-height: 1rem;
+        width: 100vw;
+    }
+</style>
