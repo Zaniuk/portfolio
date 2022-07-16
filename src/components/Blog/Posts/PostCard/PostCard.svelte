75012ca4094493b1cb6bd734c31c2508910f828b
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-07-15 21:55:28 -0300

    added dynamic links

--- src/components/Blog/Posts/PostCard/PostCard.svelte
+++ src/components/Blog/Posts/PostCard/PostCard.svelte
@@ -1,16 +1,21 @@
 <script>
-    export let date, title,img, desc
+    export let date, title,img, desc, link
     const postDate = new Date()
     const postDateStr = `${postDate.getDate()}/${postDate.getMonth()}/${postDate.getFullYear()}`
 </script>
 <div class="post-card">
-    <p class="date">{date}</p>
-    <img src={img} alt="">
-    <h1>{title}</h1>
-    <p>{desc}.</p>
+    
+        <p class="date">{date}</p>
+        <img src={img} alt="">
+        <h1><a href={link}>{title}</a></h1>
+        <p>{desc}.</p>
 </div>
 
 <style>
+    a{
+        text-decoration: none;
+        color: unset;
+    }
     img{
         aspect-ratio: 1.5 / 1;
         object-fit: cover;
